97e22450dca3ec11f9ee400eb5c7cabf31198660
yaochenglong <289911401@qq.com>
2020-08-13 08:50:22 +0800

    合并代码

---
+++ src/main/java/com/demo/bean/Cat.java
@@ -0,0 +1,18 @@
+package com.demo.bean;
+
+import com.yao.bean.Person;
+
+/**
+ * @author yaochenglong
+ * @version V1.0
+ * @date 2020/7/10 15:16
+ * @DESC
+ */
+public class Cat {
+
+    private Person person;
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+}
